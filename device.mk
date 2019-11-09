2cb3350d819fc1639ed340bcd8fe597fa57251b2
celtare21 <celtare21@gmail.com>
2019-11-09 19:09:14 +0000

    jason: Import Powerstats from Wahoo
     * HEAD: https://android.googlesource.com/device/google/wahoo/+/2de0d664d1c5de95d705c5a7a41243c393b85045
     * Remove Easel bits
     * Add manifest entry
    
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- device.mk
+++ device.mk
@@ -454,7 +454,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.pixel-libperfmgr
+    android.hardware.power@1.3-service.pixel-libperfmgr \
+    android.hardware.power.stats@1.0-service.pixel
 
 PRODUCT_COPY_FILES += \
     device/xiaomi/jason/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
